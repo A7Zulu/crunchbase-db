c378f091fc3dd7058a496c366584a8cabdf9bb24
A7Zulu <A7Zulu@gmail.com>
2014-01-08 03:33:42 +0000

    updated crunchbase.py and cruncbhase_test.py

--- crunchbase_tests.py
+++ crunchbase_tests.py
@@ -1,12 +1,22 @@
 # Crunchbase API Tests
 from crunchbase import crunchbase
-
+import redis
+import Image
+import io
 
 def main():
 	cb = crunchbase()
-	json = cb.loadJSON("/company/whistle")
+	company = "/company/whistle"
+	json = cb.loadJSON(company)
 	imageURL = cb.getImageURL(json)
 	image = cb.getImage(imageURL)
+	output = io.BytesIO()
+	image.save(output, format=image.format)
+
+	r = redis.StrictRedis(host='192.168.1.109', port=6379, db=0)
+
+	r.set(company, output.getvalue())
+	print r.get(company)
 
 
 if __name__ == '__main__':
